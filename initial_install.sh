19f9e4cb734d16d776ea6bbfd75c7f44431ea3c0
mattrices <31460714+mattrices@users.noreply.github.com>
2018-12-24 00:29:57 -0500

    Create initial_install.sh
    initial distro install script

---
+++ initial_install.sh
@@ -0,0 +1,24 @@
+sudo -s --<<EOF
+
+chmod +x update.sh
+./update.sh
+
+apt install krita
+apt install blender
+
+apt install octave
+apt install qalculate
+
+apt install clementine
+apt install audacity
+
+apt install texmaker
+apt install libre-office
+apt install libre-calc
+
+apt install gparted
+apt install brasero
+
+apt install psensor
+
+EOF
